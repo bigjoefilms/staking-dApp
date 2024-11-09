ced5a9d1881db27e8a393305a71fdd51fc09bcf6
Donatus Prince <donatusprinc@.com>
2024-11-09 09:12:38 +0100

    updated the staking config file

--- frontend/config.ts
+++ frontend/config.ts
@@ -5,25 +5,19 @@ import {
   WalletConnectConnector,
 } from "@concordium/react-components";
 
-// export const DEFAULT_CONTRACT_INDEX = BigInt(10303);
-// export const DEFAULT_CONTRACT_INDEX = BigInt(10306);
 export const DEFAULT_CONTRACT_INDEX = BigInt(10315);
 export const MAX_CONTRACT_EXECUTION_ENERGY = BigInt(30000);
-// export const VERIFIER_URL = "http://localhost:8080/api";
-// export const VERIFIER_URL =
-//   "https://techfiesta-sbt-distributor.onrender.com/api";
-// export const WEB_URL = "https://techfiesta-sbt-distributor.vercel.app";
+
 export const CONTRACT_NAME = "staking";
 export const CONTRACT_SUB_INDEX = BigInt(0);
 export const MICRO_CCD = 1000000;
 
-// export const REFRESH_INTERVAL = moment.duration(5, "seconds");
 
 const WALLET_CONNECT_OPTS = {
   projectId: CONCORDIUM_WALLET_CONNECT_PROJECT_ID,
   metadata: {
     name: "Staking Dapp",
-    description: "Example dApp",
+    description: "Stake your $CCD and get daily reward incentive",
     url: "#",
     icons: ["https://walletconnect.com/walletconnect-logo.png"],
   },
